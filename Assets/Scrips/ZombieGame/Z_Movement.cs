e2efda5deda40765eb300b2f714a2b3795723a78
sejekaj1307 <35660162+sejekaj1307@users.noreply.github.com>
2018-01-21 17:37:11 +0100

    Chef movement

--- Assets/Scrips/ZombieGame/Z_Movement.cs
+++ Assets/Scrips/ZombieGame/Z_Movement.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Z_Movement : MonoBehaviour {
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
 
-	// Use this for initialization
-	void Start () {
-		
+
+    void Update () {
+        move();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void move()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+        if (Input.GetButton("Jump"))
+            moveDirection.y = jumpSpeed;
+
+
+        transform.position = transform.position + (moveDirection * Time.deltaTime);
+
+        Vector3 targetDir = (moveDirection+transform.position) - transform.position;
+        float step = speed * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+        Debug.DrawRay(transform.position, newDir, Color.red);
+        transform.rotation = Quaternion.LookRotation(newDir);
+
+    }
 }
